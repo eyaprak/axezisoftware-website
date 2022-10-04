c2f806badc8f6085597daeeda8dbb8d2bda71c15
eyaprak <yaprakemrullah22@gmail.com>
2022-10-04 11:07:32 +0300

    card bug fixed

--- components/Cards/Card.jsx
+++ components/Cards/Card.jsx
@@ -4,7 +4,7 @@ const Card = ({ text, icon, id }) => {
     return (
         <div className={`flex-1 bg-gradient-to-r relative shadow-xl hover:-translate-y-2 transition-all duration-500 cursor-pointer overflow-hidden
         ${id == 1 ? 'from-[#242278] to-[#2A2966]' : id == 2 ? 'from-[#2A2966] to-[#1A5C6E]' : id == 3 ? 'from-[#1A5C6E] to-[#0BA4CD]' : ''} `}>
-            <div className="px-6 py-6 md:px-12 md:py-10 text-white text-2xl md:text-[28px] font-montserratExtrabold">
+            <div className="px-10 py-6 md:px-12 md:py-10 text-white text-2xl md:text-[28px] font-montserratExtrabold">
                 {text}
             </div>
             <div className="absolute top-[-88px] right-[-88px] w-[188px] h-[188px] bg-[rgba(255,255,255,0.2)] rounded-full">
