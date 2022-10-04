d6d7ec3a6954896d13a2e3a81c9f65475b1d3015
eyaprak <yaprakemrullah22@gmail.com>
2022-10-04 11:02:06 +0300

    unncessary fonts removed. Cards bug fixed

--- components/Cards/Card.jsx
+++ components/Cards/Card.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const Card = ({ text, icon, id }) => {
     return (
-        <div className={`flex-1 bg-gradient-to-r relative shadow-xl hover:-translate-y-2 transition-all duration-500 cursor-pointer
+        <div className={`flex-1 bg-gradient-to-r relative shadow-xl hover:-translate-y-2 transition-all duration-500 cursor-pointer overflow-hidden
         ${id == 1 ? 'from-[#242278] to-[#2A2966]' : id == 2 ? 'from-[#2A2966] to-[#1A5C6E]' : id == 3 ? 'from-[#1A5C6E] to-[#0BA4CD]' : ''} `}>
             <div className="px-6 py-6 md:px-12 md:py-10 text-white text-2xl md:text-[28px] font-montserratExtrabold">
                 {text}
